afea448cf8a9c9ddb47a54a9209a595f2206179d
firepick1 (localhost) <karl@firepick.org>
2015-11-08 00:03:06 -0800

    measure

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+firenodejs
+----------
+
+0.2
+---
+* GUI: Display level
+* GUI: Measure jog and LPP precision
+* GUI: version number
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@
 var controllers = angular.module('firenodejs.controllers', []);
 
 controllers.controller('firenodejs-ctrl', ['$scope', 'AlertService', 'BackgroundThread', 'firenodejs-service',
-    function(scope, alerts, bg, firenodejs) {
+    function(scope, alerts, bg, firenodej, firenodejss) {
         scope.view = {
             mainTab: "view-main"
         };
