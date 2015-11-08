fe5049ace9d99bff4b6dadc623c3589cff37aed3
firepick1 (localhost) <karl@firepick.org>
2015-11-07 21:57:52 -0800

    ...

--- js/measure.js
+++ js/measure.js
@@ -48,8 +48,8 @@ module.exports.Measure = (function() {
             for (var i=0; i < n; i++) {
                 cmd.push({movyr:dy});
             }
-            cmd.push({mov:{x:x,y:y,z:z}, dpydl:1});
-            cmd.push({mpo:"",dpyds:12,idl:500});
+            cmd.push({mov:{x:x,y:y,z:z}});
+            cmd.push({mpo:"",dpyds:12,idl:200}); // idl allows camera auto-exposure to settle
             that.firestep.send(cmd, function() {
                 console.log("jogPrecision TBD");
                 that.firesight.calcOffset(camName, function(offset) {
