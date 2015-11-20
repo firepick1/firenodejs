4490a34f9725a57834765f39c66952132ed9496e
firepick1 (localhost) <karl@firepick.org>
2015-11-20 15:24:54 -0800

    serial

--- www/js/firestep-service.js
+++ www/js/firestep-service.js
@@ -32,6 +32,7 @@ services.factory('firestep-service', ['$http', 'AlertService',
             },
             onChangeSerialPath: function() {
                 var alert = alerts.info("Establishing connection to new serial path:" + service.model.rest.serialPath);
+                service.model.initialized = false;
                 setTimeout(function() {
                     console.info("INFO\t: refreshing model due to serialPath change:", service.model.rest.serialPath);
                     service.syncModel();
