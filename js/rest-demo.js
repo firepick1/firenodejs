bc7c6690ce30f8509e635d247e98f5e716627adc
firepick1 (localhost) <karl@firepick.org>
2016-02-04 17:14:43 -0800

    ...

--- js/rest-demo.js
+++ js/rest-demo.js
@@ -20,21 +20,21 @@ for (var i=1; i < process.argv.length; i++) {
 var rest = new RestClient(restOptions);
 
 function step1() {
-    console.log("Step 1. Homing...");
+    console.log("INFO\t: Step 1. Homing...");
     rest.post("/firestep", [{hom:""},{mpo:""}], function(data) { // http response callback
-        console.log("FPD is at position x:", data.r.mpo.x, " y:", data.r.mpo.y, " z:", data.r.mpo.z);
+        console.log("INFO\t: FPD is at position x:", data.r.mpo.x, " y:", data.r.mpo.y, " z:", data.r.mpo.z);
         step2(); // do AFTER step1 is done
     });
 }
 
 function step2() {
-    console.log("Step 2. Move to (50,50)...");
+    console.log("INFO\t: Step 2. Move to (50,50)...");
     rest.post("/firestep", [{mov:{x:50,y:50}},{mpo:""}], function(data) { // http response callback
-        console.log("FPD is at position x:", data.r.mpo.x, " y:", data.r.mpo.y, " z:", data.r.mpo.z);
+        console.log("INFO\t: FPD is at position x:", data.r.mpo.x, " y:", data.r.mpo.y, " z:", data.r.mpo.z);
     });
 }
 
 step1();
 // step2(); <= DANGER! we can't just put step2 here because it will run in PARALLEL!
 
-console.log("END\t: rest-demo.js");
+console.log("END \t: rest-demo.js");
