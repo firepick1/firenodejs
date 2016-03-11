92a90016964e2ace96f9999121d9b5c7993891f8
firepick1 (localhost) <karl@firepick.org>
2016-03-11 15:57:45 -0800

    syntax

--- js/firesight/FireSightREST.js
+++ js/firesight/FireSightREST.js
@@ -4,7 +4,7 @@ var should = require("should");
 var fs = require("fs");
 var Logger = require("../../www/js/shared/Logger");
 var JsonUtil = require("../../www/js/shared/JsonUtil");
-var JsonErr = require("../../www/js/shared/JsonErr");
+var JsonError = require("../../www/js/shared/JsonError");
 
 (function(exports) {
     var verboseLogger = new Logger({
