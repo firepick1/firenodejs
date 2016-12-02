a4e5c725b079c04e96a13c2289e97613805f5f17
firepick1 (localhost) <karl@firepick.org>
2016-12-02 13:49:26 -0800

    position

--- www/js/firepaste-service.js
+++ www/js/firepaste-service.js
@@ -17,80 +17,6 @@ services.factory('firepaste-service', ['$http', 'AlertService', 'position-servic
                 return service.model.available === true;
             },
             model: {
-                kinematics: {
-                    type: "",
-                    xAxis: {
-                        name: "X-axis",
-                        icon: "glyphicon glyphicon-resize-horizontal",
-                        drive: "belt",
-                        pitch: 2,
-                        teeth: 20,
-                        steps: 200,
-                        microsteps: 16,
-                        gearout: 1,
-                        gearin: 1,
-                        mmMicrosteps: 80,
-                        homePos: 0,
-                        minPos: 0,
-                        maxPos: 200,
-                        maxHz: 18000,
-                        tAccel: 0.4,
-                        minLimit: true,
-                        maxLimit: false,
-                    },
-                    yAxis: {
-                        name: "Y-axis",
-                        icon: "glyphicon glyphicon-resize-horizontal",
-                        drive: "belt",
-                        pitch: 2,
-                        teeth: 20,
-                        steps: 200,
-                        microsteps: 16,
-                        gearout: 1,
-                        gearin: 1,
-                        mmMicrosteps: 80,
-                        homePos: 0,
-                        minPos: 0,
-                        maxPos: 200,
-                        maxHz: 18000,
-                        tAccel: 0.4,
-                        minLimit: true,
-                        maxLimit: false,
-                    },
-                    zAxis: {
-                        name: "Z-axis",
-                        icon: "glyphicon glyphicon-resize-vertical",
-                        drive: "belt",
-                        pitch: 2,
-                        teeth: 20,
-                        steps: 200,
-                        microsteps: 16,
-                        gearout: 1,
-                        gearin: 1,
-                        mmMicrosteps: 80,
-                        homePos: 0,
-                        minPos: -200,
-                        maxPos: 0,
-                        maxHz: 18000,
-                        tAccel: 0.4,
-                        minLimit: false,
-                        maxLimit: true,
-                    },
-                    bedPlane: [{
-                        x: 0,
-                        y: 0,
-                        z: 0,
-                    }, {
-                        x: 1,
-                        y: 0,
-                        z: 0,
-                    }, {
-                        x: 0,
-                        y: 1,
-                        z: 0,
-                    }],
-                    yAngle: 90,
-                },
             },
             getSyncJson: function() {
                 return service.model;
@@ -110,7 +36,7 @@ services.factory('firepaste-service', ['$http', 'AlertService', 'position-servic
                 alerts.danger("Not implemented");
             },
         };
-        service.cfgAxis = position.model.kinematics.xAxis;
+        service.cfgAxis = position.model.kinematics && position.model.kinematics.xAxis;
 
         return service;
     }
